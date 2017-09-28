44c5fc454664389fef15fe66ceadc453f7767ec4
Ranieri Teixeira <ranibtx@gmail.com>
2017-09-28 12:10:20 -0300

    a basic html template render function

---
+++ test/render.test.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var render = require('../lib/render')
+
+var fixtures = {
+  template: '<title>${title}</title><meta name="description" content="${description}"><body>${contents}</body>',
+  page: '<h1>${title}</h1><p>${pageSpecific}</p>',
+  data: {
+    title: 'Fixture Template',
+    description: 'Template test data',
+    pageSpecific: 'Page specific test data'
+  },
+  expected: '<title>Fixture Template</title><meta name="description" content="Template test data"><body><h1>Fixture Template</h1><p>Page specific test data</p></body>'
+}
+
+describe('render()', () => {
+  test('render a full HTML page', done => {
+    render(fixtures.template, fixtures.page, fixtures.data, result => {
+      expect(result).toBe(fixtures.expected)
+      done()
+    })
+  })
+})
